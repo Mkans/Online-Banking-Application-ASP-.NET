42a6e1b282cccce480054a7ad34f271729bc667f
Mkans <muthukumar.k07@gmail.com>
2018-04-19 21:20:35 -0400

    Added security question view and logic
    Note:
    Need some clean ups in the controller

--- Assignment2/Controllers/HomeController.vb
+++ Assignment2/Controllers/HomeController.vb
@@ -5,25 +5,20 @@ Namespace Controllers
     Public Class HomeController
         Inherits Controller
         Dim stringBuilder As StringBuilder
+        Dim dataContext As DataClasses2DataContext = New DataClasses2DataContext
         ' GET: Home
         Function Index() As ActionResult
             Return View()
         End Function
 
-        ' GET: Home/Details/5
-        Function Details(ByVal id As Integer) As ActionResult
-            Return View()
-        End Function
-
         ' GET: Home/Create
         Function Create() As ActionResult
-            Dim allquestion As DataClasses2DataContext = New DataClasses2DataContext
-            Dim questions = From ques In allquestion.USER_QUSTN_LSTs Select ques
-            ViewData("Questions") = sortData(questions)
+            Dim questions = From ques In DataContext.USER_QUSTN_LSTs Select ques
+            ViewData("Questions") = sortQuestions(questions)
             Return View()
         End Function
 
-        Public Function sortData(questions As IQueryable(Of USER_QUSTN_LST)) As List(Of String)
+        Public Function sortQuestions(questions As IQueryable(Of USER_QUSTN_LST)) As List(Of String)
             Dim qList As List(Of String) = New List(Of String)
             stringBuilder = New StringBuilder()
             For Each question In questions
@@ -54,57 +49,56 @@ Namespace Controllers
         Public Function Login(ByVal collection As FormCollection) As ActionResult
             Dim loginAttempt As Integer
             Try
-                If Int(Session("loginAttempt")) >= 0 Then
+                If Int(Session("loginAttempt")) >= 1 Then
                     loginAttempt = Session("loginAttempt")
                     loginAttempt = loginAttempt + 1
                     Session("loginAttempt") = loginAttempt
                 End If
             Catch ex As Exception
-                loginAttempt = 0
+                loginAttempt = 1
                 Session("loginAttempt") = loginAttempt
             End Try
-            Try
-                Dim username As String = collection("username").ToString
-                Dim password As String = collection("password").ToString
-                Dim text As String = username + password
-                Return RedirectToAction("Index")
-            Catch ex As Exception
-                Return RedirectToAction("Index")
-            End Try
+            If loginAttempt > 2 Then
+                ModelState.AddModelError("error", "You failed 3 times. Your account has been locked.")
+                Return View("Index")
+            Else
+                Try
+                    Dim username As String = collection("username").ToString
+                    Dim password As String = collection("password").ToString
+                    'Dim questions = From ques In allquestion.USER_QUSTN_LSTs Select ques
+                    Dim result = (From u In dataContext.USER_ACCOUNTs Select u Where u.USERNAME = username And u.PASSWORD = password).FirstOrDefault
+                    Dim user As USER_ACCOUNT = New USER_ACCOUNT
+                    If IsNothing(result) Then
+                        ModelState.AddModelError("error", "Invalid username/password. You have " & (3 - loginAttempt) & " left")
+                    Else
+                        'Get only the details that're already in the application ie no password and security answer
+                        user.FIRSTNAME = result.FIRSTNAME
+                        user.LASTNAME = result.LASTNAME
+                        user.USER_QA_TBs = result.USER_QA_TBs
+                        user.USERID = result.USERID
+                        Session("user") = user
+                        Dim ques = From q In dataContext.USER_QA_TBs
+                                   Join r In dataContext.USER_QUSTN_LSTs
+                                       On q.QUESTIONID Equals r.QUESTIONID
+                                   Where q.USERID = result.USERID
+                                   Select r
+
+                        Dim data As USER_QUSTN_LST = New USER_QUSTN_LST
+                        For Each q In ques
+                            data.QUESTION = q.QUESTION
+                        Next
+                        ViewData("securityQuestion") = data.QUESTION
+                    End If
+                    Return View("Security")
+                Catch ex As Exception
+                    ModelState.AddModelError("error", ex.Message)
+                    Return View("Index")
+                End Try
+            End If
         End Function
 
-        ' GET: Home/Edit/5
-        Function Edit(ByVal id As Integer) As ActionResult
-            Return View()
-        End Function
-
-        ' POST: Home/Edit/5
-        <HttpPost()>
-        Function Edit(ByVal id As Integer, ByVal collection As FormCollection) As ActionResult
-            Try
-                ' TODO: Add update logic here
-
-                Return RedirectToAction("Index")
-            Catch
-                Return View()
-            End Try
-        End Function
-
-        ' GET: Home/Delete/5
-        Function Delete(ByVal id As Integer) As ActionResult
-            Return View()
-        End Function
-
-        ' POST: Home/Delete/5
-        <HttpPost()>
-        Function Delete(ByVal id As Integer, ByVal collection As FormCollection) As ActionResult
-            Try
-                ' TODO: Add delete logic here
-
-                Return RedirectToAction("Index")
-            Catch
-                Return View()
-            End Try
+        Public Function Security(ByVal collection As FormCollection) As ActionResult
+            Return View("Index")
         End Function
     End Class
 End Namespace
---
+++ Assignment2/Views/Home/Security.vbhtml
@@ -0,0 +1,18 @@
+﻿@ModelType Assignment2.USER_QA_TB
+@Code
+	ViewData("Title") = "Security Confirmation"
+End Code
+<div class="login-page">
+	<h1 class="hometext">M.R Bank</h1>
+	@Using (Html.BeginForm("Security", "Home", FormMethod.Post))
+		@Html.AntiForgeryToken()
+		@<div Class="form">
+			@Html.ValidationSummary(False, "", New With {.class = "text-danger"})
+			<div class="login-form" action="Login" method="post">
+				<label name="securityQuestion">@ViewData("securityQuestion")</label>
+				<input type="password" placeholder="password" name="password" />
+				<Button type="submit" value="Login" name="Login"> Login</Button>
+			</div>
+		</div>
+	End Using
+</div>
